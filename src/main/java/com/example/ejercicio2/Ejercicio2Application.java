43df958393f1052c37d80bf5f39a2320709924db
DYerpesruizgijon <david.yerpes-gordillo@iesruizgijon.com>
2026-02-25 09:48:17 +0100

    intentamos arreglar esto

--- src/main/java/com/example/ejercicio2/Ejercicio2Application.java
+++ src/main/java/com/example/ejercicio2/Ejercicio2Application.java
@@ -2,8 +2,10 @@ package com.example.ejercicio2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class Ejercicio2Application {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/ejercicio2/Service/EmailService.java
+++ src/main/java/com/example/ejercicio2/Service/EmailService.java
@@ -3,6 +3,7 @@ package com.example.ejercicio2.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,6 +12,7 @@ public class EmailService {
     @Autowired
     private JavaMailSender mailSender;
 
+    @Async
     public void enviarEmailBienvenida(String destino, String nombreUsuario) {
         SimpleMailMessage mensaje = new SimpleMailMessage();
         mensaje.setTo(destino);
